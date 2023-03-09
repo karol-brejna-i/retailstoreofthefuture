f29da9c257f76b9efa860c01cb4703e0c974b007
karol-brejna-i <karolbrejna@apache.org>
2023-03-09 16:37:02 +0100

    Remove Kafka references from recommendation srvc deployment; Add TODOs to store UI README; Fix typos

--- visualization-app/app/config.py
+++ visualization-app/app/config.py
@@ -13,7 +13,7 @@ def validate_and_crash(variable, message):
 logger.info('Reading environment variables...')
 
 CUSTOMERS_LIST_FILE = os.getenv('CUSTOMERS_LIST_FILE', 'app/resources/customers.json')
-COUPONS_LIST_FILE = os.getenv('CUSTOMERS_LIST_FILE', 'app/resources/coupons.json')
+COUPONS_LIST_FILE = os.getenv('COUPONS_LIST_FILE', 'app/resources/coupons.json')
 
 MQTT_HOST = os.getenv('MQTT_HOST')
 MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
@@ -25,7 +25,9 @@ MQTT_BROKER_CERT_FILE = os.getenv('MQTT_BROKER_CERT_FILE', None)
 CUSTOMER_ENTER_TOPIC = os.getenv('ENTER_TOPIC', 'customer/enter')
 CUSTOMER_EXIT_TOPIC = os.getenv('EXIT_TOPIC', 'customer/exit')
 CUSTOMER_MOVE_TOPIC = os.getenv('MOVE_TOPIC', 'customer/move')
+# TODO previous topic constans don't use "customer_" prefix, this one does. Should be unified.
 CUSTOMER_BROWSING_TOPIC = os.getenv('CUSTOMER_BROWSING_TOPIC', 'customer/browsing')
+# TODO previous topic constans don't use "_name" suffix, this one does. Should be unified.
 COUPON_PREDICTION_TOPIC_NAME = os.getenv('COUPON_PREDICTION_TOPIC_NAME', 'customer/prediction')
 
 SCENARIO_PLAYER_SCENARIO_ENDPOINT = os.getenv('SCENARIO_PLAYER_SCENARIO_ENDPOINT')
@@ -33,3 +35,12 @@ SCENARIO_PLAYER_SCENARIO_ENDPOINT = os.getenv('SCENARIO_PLAYER_SCENARIO_ENDPOINT
 REQUIRED_PARAM_MESSAGE = 'Cannot read {} env variable. Please, make sure it is set before starting the service.'
 validate_and_crash(SCENARIO_PLAYER_SCENARIO_ENDPOINT, REQUIRED_PARAM_MESSAGE.format('SCENARIO_PLAYER_SCENARIO_ENDPOINT'))
 validate_and_crash(MQTT_HOST, REQUIRED_PARAM_MESSAGE.format('MQTT_HOST'))
+
+logger.info("Environment variables read successfully.")
+HIDDEN_CONSTANTS_KEYS = ['MQTT_PASSWORD']
+CONSTANTS_KEYS = [k for k in globals().keys() if k[0].isupper()]
+for k in CONSTANTS_KEYS:
+    value = globals().get(k) if globals().get(k) else ''
+    if k in HIDDEN_CONSTANTS_KEYS:
+        value = len(value) * "*"
+    logger.info(f'{k} = {value}')
